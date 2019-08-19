a0512fb8003e30d4a63f35bdfd70156375d2f9ec
Kevin <stradtkt22@gmail.com>
2019-08-18 22:02:16 -0400

    Added constructors

--- app/src/main/java/com/stradtman/aclass/Season.java
+++ app/src/main/java/com/stradtman/aclass/Season.java
@@ -1,16 +1,24 @@
 package com.stradtman.aclass;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Season {
-    private ArrayList<String> episodeNames;
     private int seasonNumber;
+    private ArrayList<String> episodeNames;
 
     public Season(int seasonNumber) {
         this.seasonNumber = seasonNumber;
     }
     public Season(int seasonNumber, String[] episodeNames) {
-
+        this.seasonNumber = seasonNumber;
+        this.episodeNames = new ArrayList<>(episodeNames.length);
+        this.episodeNames.addAll(Arrays.asList(episodeNames));
+    }
+    public Season(int seasonNumber, ArrayList<String> episodeNames) {
+        this.seasonNumber = seasonNumber;
+        this.episodeNames = episodeNames;
     }
 
     public String getLastEpisode() {
--- app/src/main/java/com/stradtman/aclass/TvShow.java
+++ app/src/main/java/com/stradtman/aclass/TvShow.java
@@ -6,4 +6,15 @@ public class TvShow {
     private String title, status, network, rating, airDate;
     private int imageId;
     private ArrayList<Season> seasons;
+
+    public TvShow(String title, String status, String network, String rating, String airDate, int imageId) {
+        this.title = title;
+        this.status = status;
+        this.network = network;
+        this.rating = rating;
+        this.airDate = airDate;
+        this.imageId = imageId;
+    }
+
+
 }
